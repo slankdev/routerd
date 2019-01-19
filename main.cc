f5e709c73ee9f946d152e0b785f0f4609d1cc290
Hirki Shirokura <slankdev@nttv6.jp>
2019-01-19 22:27:03 +0900

    WIP Add routerd

--- main.cc
+++ main.cc
@@ -10,6 +10,16 @@
 #include <arpa/inet.h>
 #include "yalin/yalin.h"
 
+inline static int
+routerd_monitor(const struct sockaddr_nl *who,
+         struct rtnl_ctrl_data* _dum_,
+         struct nlmsghdr *n, void *arg)
+{
+  // TODO KOKOKARA
+  printf("nlpacket\n");
+  return 0;
+}
+
 int
 main(int argc, char **argv)
 {
@@ -18,7 +28,7 @@ main(int argc, char **argv)
   if (nl == NULL)
     return 1;
 
-  int ret = netlink_listen(nl, rtnl_summary, NULL);
+  int ret = netlink_listen(nl, routerd_monitor, NULL);
   if (ret < 0)
     return 1;
 
