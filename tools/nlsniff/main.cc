b354b410c5c9e6c65521deb0393c540cc4637f51
Hiroki Shirokura <slankdev@nttv6.jp>
2019-04-22 11:42:47 +0000

    2019.04.22-11:42

--- tools/nlsniff/main.cc
+++ tools/nlsniff/main.cc
@@ -10,6 +10,7 @@
 #include <arpa/inet.h>
 #include "yalin/yalin.h"
 #include "argparse.hpp"
+#include "slankdev/filefd.h"
 
 #include <string> // std::string, std::char_traits
 #include <vector> // std::vector
@@ -28,11 +29,66 @@ namespace std {
   }
 } /* namespace  std */
 
+int monitor_from_file(const std::string& filename)
+{
+  typedef struct {
+    uint32_t ts_sec;
+    uint32_t ts_usec;
+    uint32_t incl_len;
+    uint32_t orig_len;
+  } pcap_pkt_hdr_t;
+
+  typedef struct {
+    uint32_t magic_number;
+    uint16_t version_major;
+    uint16_t version_minor;
+    int32_t  thiszone;
+    uint32_t sigfigs;
+    uint32_t snaplen;
+    uint32_t network;
+  } pcap_file_hdr_t;
+
+  typedef struct {
+    uint16_t magic;
+    uint16_t lladdr;
+    uint16_t dum1[2];
+    uint16_t dum2[2];
+    uint16_t dum3;
+    uint16_t family;
+  } nl_cooked_hdr_t;
+
+  slankdev::filefd file;
+  file.fopen(filename.c_str(), "rb");
+
+  pcap_file_hdr_t hdr;
+  file.fread(&hdr, sizeof(hdr), 1);
+  if (hdr.network != 0xfd) {
+    fprintf(stderr, "invalid network-type: %#x\n", hdr.network);
+    return 1;
+  }
+
+  while (true) {
+    if (file.feof())
+      return 0;
+
+    pcap_pkt_hdr_t pp;
+    file.fread(&pp, sizeof(pp), 1);
+
+    nl_cooked_hdr_t ph;
+    file.fread(&ph, sizeof(ph), 1);
+
+    uint8_t buf[10000];
+    file.fread(buf, pp.incl_len-sizeof(nl_cooked_hdr_t), 1);
+    rtnl_summary(NULL, NULL, (struct nlmsghdr*)buf, NULL);
+  }
+}
+
 int
 main(int argc, const char **argv)
 {
   ArgumentParser parser;
   parser.addArgument("-g", "--rtmgrp", 1, false);
+  parser.addArgument("-r", "--readfile", 1, true);
   parser.parse(argc, argv);
 
   auto grpstr = parser.retrieve<std::string>("rtmgrp");
@@ -54,15 +110,24 @@ main(int argc, const char **argv)
     else if (grps[i] == "ipv6_ifinfo") group |= RTMGRP_IPV6_IFINFO;
   }
 
-  printf("monitoring group(RTMGRP) is 0x%08x ...\n", group);
-  netlink_t* nl = netlink_open(group, NETLINK_ROUTE);
-  if (nl == NULL)
-    return 1;
+  auto readfile = parser.retrieve<std::string>("readfile");
+  if (readfile != "") {
 
-  int ret = netlink_listen(nl, rtnl_summary, NULL);
-  if (ret < 0)
-    return 1;
+    printf("read from file %s\n", readfile.c_str());
+    int ret = monitor_from_file(readfile);
+    return ret;
 
-  netlink_close(nl);
+  } else {
+    printf("monitoring group(RTMGRP) is 0x%08x ...\n", group);
+    netlink_t* nl = netlink_open(group, NETLINK_ROUTE);
+    if (nl == NULL)
+      return 1;
+
+    int ret = netlink_listen(nl, rtnl_summary, NULL);
+    if (ret < 0)
+      return 1;
+
+    netlink_close(nl);
+  }
 }
 
