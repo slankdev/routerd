b06767229fb7c769af8eb9f287f62c1671b4b531
Hiroki Shirokura <slankdev@nttv6.jp>
2019-06-20 07:52:08 +0000

    sync

--- libvty/src/lib/zebra.h
+++ libvty/src/lib/zebra.h
@@ -224,7 +224,25 @@ typedef unsigned char uint8_t;
 #define __attribute__(x)
 #endif /* !__GNUC__ || VTYSH_EXTRACT_PL */
 
-#include "zassert.h"
+extern void _zlog_assert_failed(const char *assertion, const char *file,
+				unsigned int line, const char *function)
+	__attribute__((noreturn));
+
+#if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L
+#define __ASSERT_FUNCTION    __func__
+#elif defined(__GNUC__)
+#define __ASSERT_FUNCTION    __FUNCTION__
+#else
+#define __ASSERT_FUNCTION    NULL
+#endif
+
+#define zassert(EX)                                                            \
+	((void)((EX) ? 0 : (_zlog_assert_failed(#EX, __FILE__, __LINE__,       \
+						__ASSERT_FUNCTION),            \
+			    0)))
+
+#undef assert
+#define assert(EX) zassert(EX)
 
 /*
  * Add explicit static cast only when using a C++ compiler.
