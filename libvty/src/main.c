21527f1f4f4612be50d91e8a44b6f8adacde3d4a
Hiroki Shirokura <slankdev@nttv6.jp>
2019-06-20 07:46:39 +0000

    sync

--- libvty/src/main.c
+++ libvty/src/main.c
@@ -1,13 +1,34 @@
 
 #include <zebra.h>
-#include "thread.h"
+#include "log.h"
 #include "vty.h"
+#include "prefix.h"
+#include "thread.h"
+#include "command.h"
 #include "command.h"
 #include "memory.h"
 #include "memory_vty.h"
-#include "log.h"
-#include "prefix.h"
-#include "common_cli.h"
+
+/* function to be implemented by test */
+extern void test_init(int argc, char **argv);
+
+/* functions provided by common cli
+ * (includes main())
+ */
+extern struct thread_master *master;
+
+extern int dump_args(struct vty *vty, const char *descr, int argc,
+		     struct cmd_token *argv[]);
+
+#define DUMMY_HELPSTR                                                          \
+	"00\n01\n02\n03\n04\n05\n06\n07\n08\n09\n"                             \
+	"10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n"                             \
+	"20\n21\n22\n23\n24\n25\n26\n27\n28\n29\n"
+#define DUMMY_DEFUN(name, cmdstr)                                              \
+	DEFUN(name, name##_cmd, cmdstr, DUMMY_HELPSTR)                         \
+	{                                                                      \
+		return dump_args(vty, #name, argc, argv);                      \
+	}
 
 DUMMY_DEFUN(cmd0, "slank ipv4 A.B.C.D");
 DUMMY_DEFUN(cmd1, "show int");
