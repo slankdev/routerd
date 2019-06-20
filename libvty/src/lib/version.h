9b6b9c2f90a49f1b42f66e913466cd5d9772e526
Hiroki Shirokura <slank.dev@gmail.com>
2019-06-20 15:48:04 +0000

    2019.06.20-15:48

---
+++ libvty/src/lib/version.h
@@ -0,0 +1,57 @@
+/* lib/version.h.  Generated from version.h.in by configure.
+ *
+ * Quagga version
+ * Copyright (C) 1997, 1999 Kunihiro Ishiguro
+ * 
+ * This file is part of GNU Zebra.
+ *
+ * GNU Zebra is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2, or (at your option) any
+ * later version.
+ *
+ * GNU Zebra is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Zebra; see the file COPYING.  If not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.  
+ */
+
+#ifndef _ZEBRA_VERSION_H
+#define _ZEBRA_VERSION_H
+
+#ifdef GIT_VERSION
+#include "gitversion.h"
+#endif
+
+#ifndef GIT_SUFFIX
+#define GIT_SUFFIX ""
+#endif
+#ifndef GIT_INFO
+#define GIT_INFO ""
+#endif
+
+#define FRR_PAM_NAME    "frr"
+#define FRR_SMUX_NAME   "frr"
+#define FRR_PTM_NAME    "frr"
+
+#define FRR_FULL_NAME   "FRRouting"
+#define FRR_VERSION     "7.2-dev-MyOwnFRRVersion" GIT_SUFFIX
+#define FRR_VER_SHORT   "7.2-dev-MyOwnFRRVersion"
+#define FRR_BUG_ADDRESS "https://github.com/frrouting/frr/issues"
+#define FRR_COPYRIGHT   "Copyright 1996-2005 Kunihiro Ishiguro, et al."
+#define FRR_CONFIG_ARGS "'--prefix=/usr' '--includedir=${prefix}/include' '--enable-exampledir=${prefix}/share/doc/frr/examples' '--bindir=${prefix}/bin' '--sbindir=${prefix}/lib/frr' '--libdir=${prefix}/lib/frr' '--libexecdir=${prefix}/lib/frr' '--localstatedir=/var/run/frr' '--sysconfdir=/etc/frr' '--with-moduledir=${prefix}/lib/frr/modules' '--with-libyang-pluginsdir=${prefix}/lib/frr/libyang_plugins' '--enable-configfile-mask=0640' '--enable-logfile-mask=0640' '--enable-multipath=64' '--enable-user=frr' '--enable-group=frr' '--enable-vty-group=frrvty' '--with-pkg-git-version' '--with-pkg-extra-version=-MyOwnFRRVersion'"
+
+#define FRR_DEFAULT_MOTD \
+	"\n" \
+	"Hello, this is " FRR_FULL_NAME " (version " FRR_VERSION ").\n" \
+	FRR_COPYRIGHT "\n" \
+	GIT_INFO "\n"
+
+pid_t pid_output (const char *);
+
+#endif /* _ZEBRA_VERSION_H */
