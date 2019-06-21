19967008f03d60dcf8f3aa2ed09018860b3c4e04
Hiroki Shirokura <slankdev@nttv6.jp>
2019-06-21 05:25:50 +0000

    sync

--- libvty/src/main.c
+++ libvty/src/main.c
@@ -8,10 +8,16 @@
 #include "memory.h"
 #include "memory_vty.h"
 
-/* function to be implemented by test */
-extern void test_init(int argc, char **argv);
-extern int dump_args(struct vty *vty, const char *descr, int argc,
-         struct cmd_token *argv[]);
+int dump_args(struct vty *vty,
+    const char *descr, int argc,
+    struct cmd_token *argv[])
+{
+  vty_out(vty, "%s with %d args.\n", descr, argc);
+  for (int i = 0; i < argc; i++)
+    vty_out(vty, "[%02d] %s@%s: %s\n", i, argv[i]->text,
+      argv[i]->varname, argv[i]->arg);
+  return CMD_SUCCESS;
+}
 
 #define DUMMY_HELPSTR                         \
   "00\n01\n02\n03\n04\n05\n06\n07\n08\n09\n"  \
@@ -39,92 +45,8 @@ DUMMY_DEFUN(cmd12, "alt a A.B.C.D");
 DUMMY_DEFUN(cmd13, "alt a X:X::X:X");
 DUMMY_DEFUN(cmd14, "pat g {  foo A.B.C.D$foo|foo|bar   X:X::X:X$bar| baz } [final]");
 
-/* magic_test => "magic (0-100) {ipv4net A.B.C.D/M|X:X::X:X$ipv6}" */
-DEFUN_CMD_FUNC_DECL(magic_test)
-#define funcdecl_magic_test static int magic_test_magic(\
-  const struct cmd_element *self __attribute__ ((unused)),\
-  struct vty *vty __attribute__ ((unused)),\
-  int argc __attribute__ ((unused)),\
-  struct cmd_token *argv[] __attribute__ ((unused)),\
-  long magic,\
-  const char * magic_str __attribute__ ((unused)),\
-  const struct prefix_ipv4 * ipv4net,\
-  const char * ipv4net_str __attribute__ ((unused)),\
-  struct in6_addr ipv6,\
-  const char * ipv6_str __attribute__ ((unused)))
-funcdecl_magic_test;
-DEFUN_CMD_FUNC_TEXT(magic_test)
-{
-#if 3 /* anything to parse? */
-  int _i;
-#if 1 /* anything that can fail? */
-  unsigned _fail = 0, _failcnt = 0;
-#endif
-  long magic = 0;
-  const char *magic_str = NULL;
-  struct prefix_ipv4 ipv4net = { };
-  const char *ipv4net_str = NULL;
-  struct in6_addr ipv6 = {};
-  const char *ipv6_str = NULL;
-
-  for (_i = 0; _i < argc; _i++) {
-    if (!argv[_i]->varname)
-      continue;
-#if 1 /* anything that can fail? */
-    _fail = 0;
-#endif
-
-    if (!strcmp(argv[_i]->varname, "magic")) {
-      magic_str = argv[_i]->arg;
-      char *_end;
-      magic = strtol(argv[_i]->arg, &_end, 10);
-      _fail = (_end == argv[_i]->arg) || (*_end != '\0');
-    }
-    if (!strcmp(argv[_i]->varname, "ipv4net")) {
-      ipv4net_str = argv[_i]->arg;
-      _fail = !str2prefix_ipv4(argv[_i]->arg, &ipv4net);
-    }
-    if (!strcmp(argv[_i]->varname, "ipv6")) {
-      ipv6_str = argv[_i]->arg;
-      _fail = !inet_pton(AF_INET6, argv[_i]->arg, &ipv6);
-    }
-#if 1 /* anything that can fail? */
-    if (_fail)
-      vty_out (vty, "%% invalid input for %s: %s\n",
-           argv[_i]->varname, argv[_i]->arg);
-    _failcnt += _fail;
-#endif
-  }
-#if 1 /* anything that can fail? */
-  if (_failcnt)
-    return CMD_WARNING;
-#endif
-#endif
-  if (!magic_str) {
-    vty_out(vty, "Internal CLI error [%s]\n", "magic_str");
-    return CMD_WARNING;
-  }
-
-  return magic_test_magic(self, vty, argc, argv, magic, magic_str, &ipv4net, ipv4net_str, ipv6, ipv6_str);
-}
-
-DEFPY(magic_test, magic_test_cmd,
-  "magic (0-100) {ipv4net A.B.C.D/M|X:X::X:X$ipv6}",
-  "1\n2\n3\n4\n5\n")
-{
-  char buf[256];
-  vty_out(vty, "def: %s\n", self->string);
-  vty_out(vty, "num: %ld\n", magic);
-  vty_out(vty, "ipv4: %s\n", prefix2str(ipv4net, buf, sizeof(buf)));
-  vty_out(vty, "ipv6: %s\n",
-    inet_ntop(AF_INET6, &ipv6, buf, sizeof(buf)));
-  return CMD_SUCCESS;
-}
-
 void install_commands(int argc, char **argv)
 {
-  size_t repeat = argc > 1 ? strtoul(argv[1], NULL, 0) : 223;
-
   install_element(ENABLE_NODE, &cmd0_cmd);
   install_element(ENABLE_NODE, &cmd1_cmd);
   install_element(ENABLE_NODE, &cmd2_cmd);
@@ -139,29 +61,7 @@ void install_commands(int argc, char **argv)
   install_element(ENABLE_NODE, &cmd11_cmd);
   install_element(ENABLE_NODE, &cmd12_cmd);
   install_element(ENABLE_NODE, &cmd13_cmd);
-  for (size_t i = 0; i < repeat; i++) {
-    uninstall_element(ENABLE_NODE, &cmd5_cmd);
-    install_element(ENABLE_NODE, &cmd5_cmd);
-  }
-  for (size_t i = 0; i < repeat; i++) {
-    uninstall_element(ENABLE_NODE, &cmd13_cmd);
-    install_element(ENABLE_NODE, &cmd13_cmd);
-  }
   install_element(ENABLE_NODE, &cmd14_cmd);
-  install_element(ENABLE_NODE, &magic_test_cmd);
-}
-
-int dump_args(struct vty *vty, const char *descr, int argc,
-        struct cmd_token *argv[])
-{
-  int i;
-  vty_out(vty, "%s with %d args.\n", descr, argc);
-  for (i = 0; i < argc; i++) {
-    vty_out(vty, "[%02d] %s@%s: %s\n", i, argv[i]->text,
-      argv[i]->varname, argv[i]->arg);
-  }
-
-  return CMD_SUCCESS;
 }
 
 int main(int argc, char **argv)
