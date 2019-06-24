7198a23dc4d39e91696a8659e61f0e6b2f689970
Hiroki Shirokura <slank.dev@gmail.com>
2019-06-24 04:23:08 +0000

    WIP add cli 'set interface <ip|ipv6> address'

--- vpp/vpp.c
+++ vpp/vpp.c
@@ -6,6 +6,7 @@
 #include <vnet/session/application_interface.h>
 #include <vlibmemory/api.h>
 #include <vpp/api/vpe_msg_enum.h>
+#include <vnet/api_errno.h>
 #include <svm/fifo_segment.h>
 #define vl_typedefs
 #include <vpp/api/vpe_all_api_h.h>
@@ -27,6 +28,8 @@
 #define IFC_DETAIL_MESSAGE "sw_interface_details"
 #define SET_IFC_FLAGS "sw_interface_set_flags"
 #define SET_IFC_FLAGS_REPLY "sw_interface_set_flags_reply"
+#define SET_IFC_ADDR "sw_interface_add_del_address"
+#define SET_IFC_ADDR_REPLY "sw_interface_add_del_address_reply"
 
 typedef struct
 {
@@ -92,6 +95,26 @@ set_interface_flag(u32 set_id, u32 message_id, uint32_t ifindex, bool is_up)
   mp->context = clib_host_to_net_u32(message_id);
   mp->sw_if_index = htonl(ifindex);
   mp->admin_up_down = is_up ? 1 : 0;
+  vl_msg_api_send_shmem(jm->vl_input_queue, (u8 *) &mp);
+}
+
+static int
+set_interface_addr(uint32_t set_id, uint32_t msg_id,
+    uint32_t ifindex, bool is_add, bool is_ipv6,
+    const void *addr_buffer, size_t addr_len)
+{
+  routerd_main_t *jm = &routerd_main;
+  vl_api_sw_interface_add_del_address_t *mp =
+    vl_msg_api_alloc(sizeof(*mp));
+  memset(mp, 0, sizeof(*mp));
+  mp->_vl_msg_id = ntohs(set_id);
+  mp->client_index = jm->my_client_index;
+  mp->context = clib_host_to_net_u32(msg_id);
+  mp->sw_if_index = htonl(ifindex);
+  mp->is_add = is_add;
+  mp->is_ipv6 = is_ipv6 ? 1 : 0;
+  mp->address_length = addr_len;
+  memcpy(mp->address, addr_buffer, mp->address_length);
   vl_msg_api_send_shmem(jm->vl_input_queue, (u8 *) &mp);
 }
 
@@ -169,7 +192,7 @@ DEFUN (show_vpp_interface,
   dump_ifcs(find_msg_id(DUMP_IFC_MESSAGE), 2);
   send_ping(find_msg_id(CONTROL_PING_MESSAGE), 1);
 
-  uword msg;
+  void *msg = NULL;
   api_main_t *am = &api_main;
   while (!svm_queue_sub (am->vl_input_queue, (u8 *) & msg, SVM_Q_TIMEDWAIT, 3)) {
     uint16_t msg_id = ntohs(*((uint16_t*)msg));
@@ -187,8 +210,124 @@ DEFUN (show_vpp_interface,
         mp->link_up_down > 0 ? "up" : "down", buf);
   }
 
-  vl_client_disconnect_from_vlib ();
+  vl_client_disconnect_from_vlib();
   return CMD_SUCCESS;
+}
+
+struct prefix {
+  int afi;
+  size_t plen;
+  union {
+    uint8_t raw[16];
+    struct in6_addr in6;
+    struct in_addr in4;
+  } u;
+};
+
+static void
+parse_prefix_str(const char *str, int afi, struct prefix *pref)
+{
+  assert(afi == AF_INET || afi == AF_INET6);
+  pref->afi = afi;
+
+  if (afi == AF_INET) {
+    char *pnt = strchr(str, '/');
+    if (pnt == NULL) {
+      inet_pton(AF_INET, str, &pref->u.in4);
+      pref->plen = 32;
+    } else {
+      char buf[128];
+      strncpy(buf, str, sizeof(buf));
+      buf[pnt - str] = '\0';
+      const char *plen_str = &buf[pnt - str + 1];
+      inet_pton(AF_INET, buf, &pref->u.in4);
+      pref->plen = strtol(plen_str, NULL, 0);
+    }
+  } else {
+    char *pnt = strchr(str, '/');
+    if (pnt == NULL) {
+      inet_pton(AF_INET6, str, &pref->u.in6);
+      pref->plen = 128;
+    } else {
+      char buf[128];
+      strncpy(buf, str, sizeof(buf));
+      buf[pnt - str] = '\0';
+      const char *plen_str = &buf[pnt - str + 1];
+      inet_pton(AF_INET6, buf, &pref->u.in6);
+      pref->plen = strtol(plen_str, NULL, 0);
+    }
+  }
+}
+
+inline static const char*
+vpp_vnet_strerror(int vnet_api_errno)
+{
+  switch (vnet_api_errno) {
+#define _(a,b,c) case (b): return c;
+    foreach_vnet_api_error
+#undef _
+    default: return "unknown";
+  }
+}
+
+DEFUN(set_interface_address,
+       set_interface_address_cmd,
+       "set interface < ipv4 address [del] idx NAME A.B.C.D/M | ipv6 address [del] idx NAME X:X::X:X/M >",
+       "Setting\n"
+       "Interface setting\n"
+       "Interface ipv4 address setting\n"
+       "Interface ipv4 address setting\n"
+       "Delete interface ipv4 address\n"
+       "Specify interface index\n"
+       "Specify interface index\n"
+       "Specify interface address\n"
+       "Interface ipv6 address setting\n"
+       "Interface ipv6 address setting\n"
+       "Delete interface ipv6 address\n"
+       "Specify interface index\n"
+       "Specify interface index\n"
+       "Specify interface address\n")
+{
+  bool negate = strcmp(argv[4]->arg, "del") == 0;
+  const size_t is_ipv6_pos = 2;
+  const size_t ifindex_pos = negate ? 6 : 5;
+  const size_t prefix_pos = negate ? 7 : 6;
+  bool is_ipv6 = strcmp(argv[is_ipv6_pos]->arg, "ipv6") == 0;
+  uint32_t ifindex = strtol(argv[ifindex_pos]->arg, NULL, 0);
+  const char *pref_str = argv[prefix_pos]->arg;
+
+  struct prefix prefix;
+  parse_prefix_str(pref_str,
+      is_ipv6 ? AF_INET6 : AF_INET, &prefix);
+
+  if (connect_to_vpp("routerd", true) < 0) {
+    svm_region_exit ();
+    vty_out(vty, "Couldn't connect to vpe, exiting...\n");
+    return CMD_WARNING_CONFIG_FAILED;
+  }
+
+  set_interface_addr(find_msg_id(SET_IFC_ADDR), 3,
+      ifindex, !negate, is_ipv6, prefix.u.raw, prefix.plen);
+
+  void *msg = NULL;
+  api_main_t *am = &api_main;
+  while (!svm_queue_sub (am->vl_input_queue, (u8 *) & msg, SVM_Q_TIMEDWAIT, 1)) {
+    vl_api_sw_interface_add_del_address_reply_t * mp =
+      (vl_api_sw_interface_add_del_address_reply_t*)msg;
+    int32_t retval = ntohl(mp->retval);
+    if (retval < 0) {
+      vty_out(vty, "setting was failed: %s (%d)\n",
+          vpp_vnet_strerror(retval), retval);
+      vl_client_disconnect_from_vlib ();
+      return CMD_WARNING_CONFIG_FAILED;
+    }
+    vl_client_disconnect_from_vlib();
+    return CMD_SUCCESS;
+  }
+
+  vty_out(vty, "timeout\n");
+  vl_client_disconnect_from_vlib ();
+  return CMD_WARNING_CONFIG_FAILED;
 }
 
 DEFUN(set_interface_state,
@@ -212,14 +351,16 @@ DEFUN(set_interface_state,
 	bool is_up = strcmp(argv[5]->arg, "up") == 0 ? true : false;
   set_interface_flag(find_msg_id(SET_IFC_FLAGS), 2, ifindex, is_up);
 
+  void *msg2;
   api_main_t *am = &api_main;
-  uword msg2;
+
   while (!svm_queue_sub (am->vl_input_queue, (u8 *) & msg2, SVM_Q_TIMEDWAIT, 3)) {
     vl_api_sw_interface_set_flags_reply_t * mp =
       (vl_api_sw_interface_set_flags_reply_t*)msg2;
     int32_t retval = ntohl(mp->retval);
     if (retval < 0) {
-      vty_out(vty, "setting was failed retval=%d\n", retval);
+      vty_out(vty, "setting was failed: %s (%d)\n",
+          vpp_vnet_strerror(retval), retval);
       vl_client_disconnect_from_vlib ();
       return CMD_WARNING_CONFIG_FAILED;
     }
@@ -274,6 +415,10 @@ setup_vpp_node(vui_t *vui)
   vui_install_element(vui, vpp_node->node, &vpe_connect_cmd);
   vui_install_element(vui, vpp_node->node, &vpe_disconnect_cmd);
 
+  // TODO: change node ENABLE_NODE -> vpp_node->node
+  vui_install_element(vui, ENABLE_NODE, &set_interface_address_cmd);
   vui_install_element(vui, ENABLE_NODE, &set_interface_state_cmd);
+  /* vui_install_element(vui, ENABLE_NODE, &create_loopback_interface); */
+  /* vui_install_element(vui, ENABLE_NODE, &add_route); */
 }
 
