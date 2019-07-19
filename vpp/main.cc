cee3ef33191b5904933c31b0ad02410e85469b08
Hiroki Shirokura <slankdev@nttv6.jp>
2019-07-19 09:13:36 +0000

    Support command-line-option c,i

--- vpp/main.cc
+++ vpp/main.cc
@@ -1,5 +1,6 @@
 
 #include <stdio.h>
+#include <getopt.h>
 #include <thread>
 #include <vui/vui.h>
 
@@ -8,32 +9,81 @@
 #include "vpp_cli.h"
 #include "tap.h"
 #include "debug.h"
+#include "routerd.h"
 
-void th1()
+static void
+vui_manager(bool interactive, const char* config_path)
 {
   vui_t *vui = vui_new();
   vui_set_password(vui, "slank");
   vui_set_hostname(vui, "slank");
   vui_serv_sock(vui, NULL, 9077);
-  // vui_serv_stdio(vui);
+  if (interactive)
+    vui_serv_stdio(vui);
   setup_netlink_node(vui);
   setup_vpp_node(vui);
   setup_tap_node(vui);
   setup_debug_node(vui);
-  vui_read_conf(vui, "./routerd.conf");
+  vui_read_conf(vui, config_path);
 
   vui_run();
   vui_delete(vui);
 }
 
+static void
+usage(const char *progname)
+{
+  FILE *fp = stdout;
+  fprintf(fp, "Usage: %s\n", progname);
+}
+
+static int
+parse_args(int argc, char **argv)
+{
+  const static struct option longopts[] = {
+      { "interactive", no_argument, NULL, 'i' },
+      { "configfile", required_argument, NULL, 'c' },
+      { 0, 0, 0, 0 },
+  };
+
+  while (true) {
+    int opt_index;
+    int opt = getopt_long(argc, argv, "ic:", longopts, &opt_index);
+    if (opt == EOF)
+      break;
+
+    switch (opt) {
+      case 'i':
+        rd_ctx.global_config.enable_interactive = true;
+        break;
+      case 'c':
+        rd_ctx.global_config.config_path = optarg;
+        break;
+        break;
+      default:
+        fprintf(stdout, "%s: invalid arg \'%c\'\n", __func__, opt);
+        return -1;
+        break;
+    }
+  }
+  return 0;
+}
+
 int main(int argc, char **argv)
 {
-  std::thread thrd1(th1);
+  int ret = parse_args(argc, argv);
+  if (ret < 0) {
+    usage(argv[0]);
+    return 1;
+  }
+  // rd_ctx.dump(stdout);
+
+  std::thread thrd1(vui_manager,
+      rd_ctx.global_config.enable_interactive,
+      rd_ctx.global_config.config_path.c_str());
   sleep(1); //XXX
   std::thread thrd2(netlink_manager);
   thrd1.join();
   thrd2.join();
-  // pthread_join(thread1, NULL);
-  // pthread_join(thread2, NULL);
 }
 
--- vpp/routerd.h
+++ vpp/routerd.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <vector>
+#include <string>
 
 struct routerd_interface {
   uint32_t kern_ifindex;
@@ -13,10 +14,32 @@ struct routerd_interface {
 
 struct routerd_context {
   std::vector<struct routerd_interface> interfaces;
+
+  struct {
+    bool enable_interactive;
+    std::string config_path;
+  } global_config;
+
+  routerd_context()
+  {
+    global_config.enable_interactive = false;
+    global_config.config_path = "/etc/routerd/routerd.conf";
+  }
+
   void add_interface(uint32_t k, uint32_t v)
   {
     routerd_interface iface(k, v);
     interfaces.push_back(iface);
+  }
+
+  void dump(FILE *fp) const
+  {
+    fprintf(fp, "==========================================\r\n");
+    fprintf(fp, "global_config.enable_interactive: %s\r\n",
+        global_config.enable_interactive ? "true" : "false");
+    fprintf(fp, "global_config.config_path: %s\r\n",
+        global_config.config_path.c_str());
+    fprintf(fp, "==========================================\r\n");
   }
 };
 
