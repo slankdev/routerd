acb5ea75b93b7f90be0adb7901c82cec96ae58da
Hiroki Shirokura <hiroki.shirokura@linecorp.com>
2019-12-23 10:58:48 +0900

    2019.12.23-10:58

--- netlink_scripts/nexthop/main.c
+++ netlink_scripts/nexthop/main.c
@@ -9,10 +9,14 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <linux/nexthop.h>
+#include <linux/lwtunnel.h>
+#include <linux/seg6.h>
+#include <linux/seg6_local.h>
+#include <linux/seg6_iptunnel.h>
 
 static void adddel_nexthop(int fd,
-		uint32_t oif_idx, uint32_t nh_id,
-		bool install)
+    uint32_t oif_idx, uint32_t nh_id,
+    bool install)
 {
   struct {
     struct nlmsghdr  n;
@@ -22,24 +26,35 @@ static void adddel_nexthop(int fd,
     .n.nlmsg_len = NLMSG_LENGTH(sizeof(struct nhmsg)),
     .n.nlmsg_flags = NLM_F_REQUEST | NLM_F_CREATE | NLM_F_EXCL | NLM_F_ACK,
     .n.nlmsg_type = install ? RTM_NEWNEXTHOP : RTM_DELNEXTHOP,
-    .nh.nh_family = AF_INET,
+    .nh.nh_family = AF_INET6,
     .nh.nh_scope = 0x00,
     .nh.nh_protocol = 0x03,
-		.nh.resvd = 0x00,
+    .nh.resvd = 0x00,
     .nh.nh_flags = 0,
   };
 
-	addattr32(&req.n, sizeof(req), NHA_OIF, oif_idx);
-	addattr32(&req.n, sizeof(req), NHA_ID, nh_id);
+  addattr32(&req.n, sizeof(req), NHA_OIF, oif_idx);
+  addattr32(&req.n, sizeof(req), NHA_ID, nh_id);
 
-	char answer[1000] = {0};
+  char buf[1024];
+  struct rtattr *rta = (void *)buf;
+  rta->rta_type = NHA_ENCAP;
+  rta->rta_len = RTA_LENGTH(0);
+  struct rtattr *nest = rta_nest(rta, sizeof(buf), NHA_ENCAP);
+  rta_addattr32(rta, sizeof(buf), SEG6_LOCAL_ACTION, SEG6_LOCAL_ACTION_END);
+  rta_nest_end(rta, nest);
+  addraw_l(&req.n, 1024 , RTA_DATA(rta), RTA_PAYLOAD(rta));
+
+  addattr16(&req.n, sizeof(req), NHA_ENCAP_TYPE, LWTUNNEL_ENCAP_SEG6_LOCAL);
+
+  char answer[1000] = {0};
   hexdump(stdout, &req.n, req.n.nlmsg_len);
-	int ret = nl_talk(fd, &req.n, (void*)answer, sizeof(answer));
+  int ret = nl_talk(fd, &req.n, (void*)answer, sizeof(answer));
   if (ret < 0)
-		exit(1);
+    exit(1);
 
-	printf("==========\n");
-	hexdump(stdout, answer, ret);
+  printf("==========\n");
+  hexdump(stdout, answer, ret);
 }
 
 void main() {
@@ -48,12 +63,12 @@ void main() {
     exit(1);
 
 #define SOL_NETLINK 270
-	const int one = 1;
-	int ret = setsockopt(fd, SOL_NETLINK, NETLINK_EXT_ACK, &one, sizeof(one));
-	if (ret < 0) {
-		perror("setsockopt(NETLINK_F_EXT_ACK)");
-		exit(1);
-	}
+  const int one = 1;
+  int ret = setsockopt(fd, SOL_NETLINK, NETLINK_EXT_ACK, &one, sizeof(one));
+  if (ret < 0) {
+    perror("setsockopt(NETLINK_F_EXT_ACK)");
+    exit(1);
+  }
 
   uint32_t oif_idx = 4;
   uint32_t nh_id = 22;
